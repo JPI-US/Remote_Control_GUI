1b35908dc2d8de78fb97838788f382daa2575697
SimonAsenime <omateolashi-sa@proton.me>
2023-08-29 11:18:36 -0500

    Major website changes

--- src/App.js
+++ src/App.js
@@ -1,80 +1,98 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Registry, Client } from 'azure-iothub';
+import "./App.css";
 
+export const HomePage = () => {
+  const [state, setState] = useState("Startup");
+  const [method, setMethod] = useState("Restart");
+  const [deviceID, setDeviceID] = useState("E-6")
+  const [querying, setQuerying] = useState(false);
 
-var connectionString = 'HostName=EspTest.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1KTLsTs2R80sKTPlK9TP+5u/yGcQBuni43NRTxU4vpQ=';
-var registry = Registry.fromConnectionString(connectionString);
-var client = Client.fromConnectionString(connectionString);
-var deviceToReboot = 'E-6';
+  var connectionString = 'HostName=EspTest.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1KTLsTs2R80sKTPlK9TP+5u/yGcQBuni43NRTxU4vpQ=';
+  var registry = Registry.fromConnectionString(connectionString);
+  var client = Client.fromConnectionString(connectionString);
 
-var state = 0;
+  var directMethod = (twin) => {
 
-var startRebootDevice = function(twin) {
+    var methodParams = {
+        methodName: method,
+        payload: null,
+        timeoutInSeconds: 60,
+        connectTimeoutInSeconds: 30
+    };
 
-  var methodName = "Restart";
-
-  var methodParams = {
-      methodName: methodName,
-      payload: null,
-      timeoutInSeconds: 60,
-      connectTimeoutInSeconds: 30
+    client.invokeDeviceMethod(deviceID, methodParams, function(err, result) {
+        if (err) {
+            console.error("Direct method error: "+err.message);
+        } else {
+            console.log("Successfully invoked the device to reboot.");
+            console.log(result);
+        }
+    });
   };
 
-  client.invokeDeviceMethod(deviceToReboot, methodParams, function(err, result) {
-      if (err) {
-          console.error("Direct method error: "+err.message);
-      } else {
-          console.log("Successfully invoked the device to reboot.");
-          console.log(result);
-      }
-  });
-};
+  var getState = (val) => {
+    switch (val) {
+      case 0:
+        return "Startup";
+      case 1:
+        return "Connection";
+      case 2:
+        return "Health Check";
+      case 3:
+        return "Sun Tracking";
+      case 4:
+        return "Idle";
+      case 5:
+        return "Night";
+      case 6:
+        return "Maintenance";
+      default:
+        return "Idle";
+    }
+  }
 
-var queryTwinLastReboot = function() {
+  useEffect(() => {
+    var queryTwinLastReboot = () => {
 
-  registry.getTwin(deviceToReboot, function(err, twin){
-
-      if (twin.properties.reported.towerState != null)
-      {
-          if (err) {
-              console.error('Could not query twins: ' + err.constructor.name + ': ' + err.message);
-          } else {
-              var state = twin.properties.reported.towerState;
-              console.log('Tower State: ' + state);
-          }
-      } else 
-          console.log('Waiting for state.');
-  });
-};
-
-function HomePage() {
-  setInterval(queryTwinLastReboot, 2000);
-
-  const buttonStyle = {
-    alignSelf: 'flex-start',
-    marginLeft: '2rem',
-    padding: '0.5rem 1rem',
-    border: '2px solid #333',
-    backgroundColor: 'white',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    fontSize: '1rem',
-    color: 'black', // Add this line to set the text color to black
-  };
-
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '100vh' }}>
-      <h1 style={{ color: 'yellow', fontSize: '4rem', marginBottom: '1rem' }}>Janta Remote Control</h1>
-      <button style={buttonStyle} onClick={startRebootDevice} >Restart</button>
-      <button style={buttonStyle}>East</button>
-      <button style={buttonStyle}>West</button>
-      <button style={buttonStyle}>Stop</button>
-      <button style={buttonStyle}>Maintenance</button>
-      <button style={buttonStyle}>End Maintenance</button>
-      <button style={buttonStyle}>Reset</button>
-      <h3>{state}</h3>
+      registry.getTwin(deviceID, function(err, twin){
+  
+          if (twin.properties.reported.towerState != null)
+          {
+              if (err) {
+                  console.error('Could not query twins: ' + err.constructor.name + ': ' + err.message);
+              } else {
+                  setState(getState(twin.properties.reported.towerState));
+              }
+          } else 
+              console.log('Waiting for state.');
+      });
+    };
+    if (!querying) {
+      setInterval(queryTwinLastReboot, 5000);
+      setQuerying(true);
+    }
+  }, [registry]);
+  console.log(method);
+  return <>
+    <h1>Janta Remote Control</h1>
+    <div className="content">
+      <select onChange={e => {setMethod(e.target.value)}} className="buttons" name="controls" id="controls">
+        <option value="Restart">Restart</option>
+        <option value="East">East</option>
+        <option value="West">West</option>
+        <option value="Stop">Stop</option>
+        <option value="Maintenance">Maintenance</option>
+        <option value="End Maintenance">End Maintenance</option>
+        <option value="Reset">Reset</option>
+      </select>
+      <label className='buttons' for="fname">Device ID:</label>
+      <input className='buttons' onChange={e => {setDeviceID(e.target.value)}} type="text" id="fname" name="fname"/> 
+      <button className="buttons" onClick={directMethod} >Submit</button>
+      <h3 className='state'>Tower State: {state}</h3>
     </div>
-  );
+  </>;
 }
 
-export default HomePage;
+//export default HomePage;
