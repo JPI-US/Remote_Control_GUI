1376821c98ee279f50d53c743db60dcf6d1c91c4
SimonAsenime <omateolashi-sa@proton.me>
2023-09-13 15:19:01 -0500

    Add service worker support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HomePage } from './App.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<HomePage />, document.getElementById('root'));
+serviceWorkerRegistration.register();
