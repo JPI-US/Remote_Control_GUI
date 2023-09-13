dd33db959db7a40fdadedf7fcb386724914a6d1d
SimonAsenime <omateolashi-sa@proton.me>
2023-09-13 11:47:42 -0500

    new installations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HomePage } from './App';
+import { HomePage } from './App.jsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<HomePage />, document.getElementById('root'));
